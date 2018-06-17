58773c16004984ec192f33c9b7222eb7287a7d86
Enrico Jr Tuvera <enrico@hashtag-interactive.com>
2018-06-17 11:03:51 +0800

    - imported templates from the .gitignore repo. - created a setup.py file to turn it into a proper python package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import *
+
+setup(name='ignoramus',
+	version='0.1',
+	description='A command line tool for generating .gitignore files.',
+	url='https://github.com/cornjuliox/ignoramus',
+	author='Enrico Jr Tuvera',
+	author_email='enricojr.tuvera@gmail.com',
+	packages=['ignoramus'],
+	zip_safe=False)
