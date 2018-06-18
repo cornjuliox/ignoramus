1efbb8e43ff0b276164553387dbb06b6a6caf729
Enrico Jr Tuvera <enrico@hashtag-interactive.com>
2018-06-18 16:50:08 +0800

    up up and awayyy

--- setup.py
+++ setup.py
@@ -6,9 +6,12 @@ setup(name='ignoramus',
 	url='https://github.com/cornjuliox/ignoramus',
 	author='Enrico Jr Tuvera',
 	author_email='enricojr.tuvera@gmail.com',
-	packages=['ignoramus'],
+	packages=find_packages(),
 	install_requires=[
 		'click==6.7'
 	],
-	scripts=['ignoramus/ignoramus.py'],
-	zip_safe=False)
+	zip_safe=False,
+	entry_points='''
+		[console_scripts]
+		ignoramus=ignoramus:supermain
+	''')
